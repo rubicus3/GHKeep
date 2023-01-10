03392eb3470653d8a754f44052482ca18ba17146
rubicus <laba625@yandex.ru>
2023-01-10 13:12:39 +0300

    Basic navigation

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Flex, Spacer } from "react-native-flex-layout";
+import HomeScreen from "./Screens/Home";
+import SettingsScreen from "./Screens/Settings";
+import ManageScreen from "./Screens/Manage";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>WELCOME TO YOUR DOOM!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{ header: () => null }}
+                />
+                <Tab.Screen
+                    name="Manage"
+                    component={ManageScreen}
+                    options={{ header: () => null }}
+                />
+                <Tab.Screen
+                    name="Settings"
+                    component={SettingsScreen}
+                    options={{ header: () => null }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Screens/Manage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function ManageScreen() {
+    return (
+        <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+            <Text>Manage!</Text>
+        </View>
+    );
+}

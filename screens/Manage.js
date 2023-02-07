fa02b3a7bbc554fbdf312af694205ab6406d5424
rubicus <laba625@yandex.ru>
2023-02-07 10:15:51 +0300

    Manage layout

--- screens/Manage.js
+++ screens/Manage.js
@@ -1,10 +1,25 @@
 import { Platform, ScrollView, View } from "react-native";
-import THStatuss from '../components/THStatus';
+import { Text, Divider } from "react-native-paper";
+import THStatus from "../components/THStatus";
+import { useTheme } from "react-native-paper";
 
 export default function ManageScreen() {
+    const theme = useTheme();
+
     return (
         <ScrollView scrollEnabled={false}>
-            <THStatuss />
+            <Text variant="headlineMedium" style={{ padding: 15 }}>
+                Статус
+            </Text>
+
+            <Divider />
+            <THStatus />
+            <THStatus status={false} icon="water-sync" text="Полив"/>
+            <THStatus />
+            <THStatus />
+            <THStatus />
+            <THStatus />
+            <THStatus />
         </ScrollView>
     );
 }
